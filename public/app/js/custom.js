6ae755d4e2b04ca0264fe4c596a2d2d62892a9ec
ajosepha <ariel.isaacs@gmail.com>
2014-02-27 22:47:40 -0500

    fix formatting and add jquery that works in the console

--- public/app/js/custom.js
+++ public/app/js/custom.js
@@ -1,11 +1,26 @@
+// $(document).ready(function(){
+//   setTimeout(function(){
+//     $(".hormone").hide();
+//   }, 1000);
+//   $(".col6").on("click", function(){
+//     $(this).children().show("slow", function(){
+
+//     });
+//     // $(this).find(".hormone").show()
+//   });
+
+
+// });
+
+// // $(".col6").on("click", function(){alert("hi")})
+// $(".col6").on("click", function(){$(this).find(".hormone").show()})
+
 $(document).ready(function(){
   setTimeout(function(){
     $(".hormone").hide();
   }, 1000);
+  $(".card").on("click", function(alert){
+    $(this).find(".hormone").fadeIn()
+  });
   
-  $(".col6").on("click", function(){
-    $(this).find(".hormone").show()
-  })
-
-
 });
