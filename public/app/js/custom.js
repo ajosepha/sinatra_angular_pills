900bb982e6c12890d209a781630dfdd4dce7efb2
ajosepha <ariel.isaacs@gmail.com>
2014-02-28 09:17:49 -0500

    add workign javascript

--- public/app/js/custom.js
+++ public/app/js/custom.js
@@ -1,26 +1,13 @@
-// $(document).ready(function(){
-//   setTimeout(function(){
-//     $(".hormone").hide();
-//   }, 1000);
-//   $(".col6").on("click", function(){
-//     $(this).children().show("slow", function(){
 
-//     });
-//     // $(this).find(".hormone").show()
-//   });
-
-
-// });
-
-// // $(".col6").on("click", function(){alert("hi")})
-// $(".col6").on("click", function(){$(this).find(".hormone").show()})
 
 $(document).ready(function(){
   setTimeout(function(){
     $(".hormone").hide();
+    $(".card").on("click", function(alert){
+      $(this).find(".hormone").fadeIn()
+    });
   }, 1000);
-  $(".card").on("click", function(alert){
-    $(this).find(".hormone").fadeIn()
-  });
+
+  
   
 });
