26de0bb89eaa7d7ef38a5cf741a3443c769fedbd
ajosepha <ariel.isaacs@gmail.com>
2014-03-03 10:14:49 -0500

    remove excess code

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -9,21 +9,8 @@ pillcatApp.controller('PillListCtrl', ['$scope', '$http',
       $http.get('/pills').success(function(data){
         $scope.pills = data;
       });
-    // $scope.pills = Pillcat.query();
+
     
     }]);
 
-  // function($scope, $http) {
-  // $http.get('pills/pills.json').success(function(data){
-  //   $scope.pills = data
-  // });
-
-// });
-
-// pillcatApp.controller('PillDetailCtrl', ['$scope', '$routeParams', '$http',
-//   function($scope, $routeParams, $http) {
-//     $http.get("pills/pills-" + '.json').success(function(data) {
-//       $scope.pill = data[0];
-//     }); 
-//   }]);
 
